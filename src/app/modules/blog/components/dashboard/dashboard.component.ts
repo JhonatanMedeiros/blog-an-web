f33cb450d120eb3fde9f312b4be0504be5ff358b
Jhonatan <jhonatan.medeiros02@gmail.com>
2017-10-15 18:19:46 -0200

    Adicionado componente de lista de ultimas postagens na dashboard do Modulo Blog

--- src/app/modules/blog/components/dashboard/dashboard.component.ts
+++ src/app/modules/blog/components/dashboard/dashboard.component.ts
@@ -9,12 +9,40 @@ export class DashboardComponent implements OnInit {
 
   links = [
     {text: 'Inicio', link: '/'},
-    {text: 'News', link: '/posts'},
-    {text: 'Categoria 1', link: '/category1'},
-    {text: 'Categoria 2', link: '/category2'},
+    {text: 'Recentes', link: '/posts'},
+    {text: 'Angular 2', link: '/category1'},
+    {text: 'Node.js', link: '/category2'}
     ];
 
-  constructor() { }
+  posts_List = [];
+
+  constructor() {
+
+    let obj1 = {
+      id: 1,
+      url: '/blog',
+      title: 'Posttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt 1',
+      imgUrl: 'http://material.angular.io/assets/img/examples/shiba2.jpg',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci animi architecto assumenda, delectus eligendi est et eum expedita fugiat fugit impedit itaque nulla odio omnis saepe sint tenetur veniam voluptatem?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci animi architecto assumenda, delectus eligendi est et eum expedita fugiat fugit impedit itaque nulla odio omnis saepe sint tenetur veniam voluptatem?',
+      author: 'Nome',
+      authorId: 1
+    };
+    let obj2 = {
+      id: 2,
+      url: '/blog',
+      title: 'Post 2',
+      imgUrl: 'http://material.angular.io/assets/img/examples/shiba2.jpg',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci animi architecto assumenda, delectus eligendi est et eum expedita fugiat fugit impedit itaque nulla odio omnis saepe sint tenetur veniam voluptatem?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci animi architecto assumenda, delectus eligendi est et eum expedita fugiat fugit impedit itaque nulla odio omnis saepe sint tenetur veniam voluptatem?',
+      author: 'Nome',
+      authorId: 2
+    };
+
+    this.posts_List.push(obj1);
+    this.posts_List.push(obj1);
+    this.posts_List.push(obj2);
+    this.posts_List.push(obj1);
+    this.posts_List.push(obj2);
+  }
 
   ngOnInit() {
   }
