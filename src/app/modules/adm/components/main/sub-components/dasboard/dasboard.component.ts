9314748788aa44534c6753d4135cf5df3467f978
Jhonatan <jhonatan.medeiros02@gmail.com>
2017-11-04 11:25:17 -0200

    Remoção de obj de post.

--- src/app/modules/adm/components/main/sub-components/dasboard/dasboard.component.ts
+++ src/app/modules/adm/components/main/sub-components/dasboard/dasboard.component.ts
@@ -32,27 +32,6 @@ export class DasboardComponent implements OnInit {
 
   constructor(private route: Router) {
 
-    let obj1 = {
-        id: 1,
-        url: '/blog',
-        title: 'Posttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt 1',
-        imgUrl: 'http://material.angular.io/assets/img/examples/shiba2.jpg',
-        description: 'dsadsa',
-        author: 'Nome',
-        authorId: 1
-      };
-    let obj2 = {
-      id: 2,
-      url: '/blog',
-      title: 'Post 2',
-      imgUrl: '',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci animi architecto assumenda, delectus eligendi est et eum expedita fugiat fugit impedit itaque nulla odio omnis saepe sint tenetur veniam voluptatem?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci animi architecto assumenda, delectus eligendi est et eum expedita fugiat fugit impedit itaque nulla odio omnis saepe sint tenetur veniam voluptatem?',
-      author: 'Nome',
-      authorId: 2
-    };
-
-    this.posts_List.push(obj1);
-    this.posts_List.push(obj2);
   }
 
   ngOnInit() {
